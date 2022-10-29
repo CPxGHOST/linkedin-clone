ca6884fa8372b90953dff3b5fc50f801a93b58e5
CPxGHOST <mridul2598@gmail.com>
2022-10-29 23:47:46 +0530

    Added upwork header

--- linkedin/src/pages/home/home.tsx
+++ linkedin/src/pages/home/home.tsx
@@ -1,3 +1,15 @@
+import styles from "./home.module.css";
 export const Home = () => {
-    return <div>Home</div>
-}
+  return (
+    <div className={styles.container}>
+      <section className={styles["heading-section"]}>
+        <h5>
+          <a>Hiring in a hurry? -</a>
+        </h5>
+        <p>
+          Find talented pros in record time with Upwork and keep business moving
+        </p>
+      </section>
+    </div>
+  );
+};
--- linkedin/src/shared/login-navbar/loggedInNavbar.tsx
+++ linkedin/src/shared/login-navbar/loggedInNavbar.tsx
@@ -1,15 +1,84 @@
 import styles from "./loggedInNavbar.module.css";
 import homeLogo from "../../assets/svg/home-logo.svg";
 import { Link } from "react-router-dom";
+import searchIcon from "../../assets/svg/search-icon.svg";
+import navHome from "../../assets/svg/nav-home.svg";
+import navJobs from '../../assets/svg/nav-jobs.svg';
+import navMessaging from '../../assets/svg/nav-messaging.svg';
+import navNetwork from '../../assets/svg/nav-network.svg';
+import navNotification from '../../assets/svg/nav-notifications.svg';
+import navWork from '../../assets/svg/nav-work.svg';
+import user from '../../assets/svg/user.svg';
+import downIcon from '../../assets/svg/down-icon.svg';
+import styled from "styled-components";
 export const LoggedinNavbar = () => {
   return (
     <header className={styles.header}>
       <div className={styles.content}>
         <span className={styles.logo}>
-          <Link to={'/home'}>
-            <img src={homeLogo} />
+          <Link to={"/home"}>
+            <img src={homeLogo} alt="" />
           </Link>
         </span>
+        <div className={styles.search}>
+          <div>
+            <input type={"text"} placeholder="Search" />
+          </div>
+          <div className={styles["search-icon"]}>
+            <img src={searchIcon} alt="" />
+          </div>
+        </div>
+        <nav className={styles.nav}>
+          <ul className={styles["navlist-wrap"]}>
+            <li className={`${styles.navlist} ${styles.active}`}>
+              <Link to={"/home"}>
+                <img src={navHome} alt="" />
+                <span>Home</span>
+              </Link>
+            </li>
+            <li className={styles.navlist}>
+              <Link to={"/home"}>
+                <img src={navNetwork} alt="" />
+                <span>My Network</span>
+              </Link>
+            </li>
+            <li className={styles.navlist}>
+              <Link to={"/home"}>
+                <img src={navJobs} alt="" />
+                <span>Jobs</span>
+              </Link>
+            </li>
+            <li className={styles.navlist}>
+              <Link to={"/home"}>
+                <img src={navMessaging} alt="" />
+                <span>Messaging</span>
+              </Link>
+            </li>
+            <li className={styles.navlist}>
+              <Link to={"/home"}>
+                <img src={navNotification} alt="" />
+                <span>Notifications</span>
+              </Link>
+            </li>
+            <li className={`${styles.navlist} ${styles.user}`}>
+                <Link to={''}>
+                    <img src={user} alt="" />
+                    <span>Me</span>
+                    <img src={downIcon} alt="" />
+                </Link>
+                <div className={styles.signout}>
+                    Signout
+                </div>
+            </li>
+            <li className={`${styles.navlist} ${styles.work}`}>
+                <Link to={''}>
+                    <img src={navWork} alt="" />
+                    <span>Work</span>
+                    <img src={downIcon} alt="" />
+                </Link>
+            </li>
+        </ul>
+        </nav>
       </div>
     </header>
   );
